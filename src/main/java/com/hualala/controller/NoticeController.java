6a3c2feabd7e936707ca5342909fabaab2a12917
yuanchong <yuanchong@hualala.com>
2019-06-07 20:13:26 +0800

    controller修改

--- src/main/java/com/hualala/controller/NoticeController.java
+++ src/main/java/com/hualala/controller/NoticeController.java
@@ -9,13 +9,13 @@ import com.hualala.exception.BusinessException;
 import com.hualala.service.NoticeService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/notice")
 public class NoticeController {
 	
